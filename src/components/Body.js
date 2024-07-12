a98f4450da42ea78e7aed59dd6c02887da13003d
jeetesh <release123>
2024-07-12 21:50:10 +0530

    day10

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../../src/components/Button";
 import { swiggy } from "../utils/mockData.js";
 import RestroCards from "./RestroCards";
 
 export const Body = () => {
   const [list, setList] = useState(swiggy);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8890014&lng=77.5984758&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        console.log(response);
+      } catch (e) {
+        console.log(e, "Data fetching Failed");
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="body">
       <div className="search">
